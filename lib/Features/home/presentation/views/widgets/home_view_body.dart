a68abaaeaba442a7246fb5bf3c0f3f364ddb59f2
Mortadha Naser <mortadha114@gmail.com>
2023-06-19 21:24:58 +0300

    add custom app bar

---
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import 'custom_app_bar.dart';
+
+class HomeViewBody extends StatelessWidget {
+  const HomeViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+        CustomAppBar(),
+      ],
+    );
+  }
+}
+
+
