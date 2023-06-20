5078964817cbcca06abc5202ea5d152f1f096d28
mortadha1144 <mortadha114@gmail.com>
2023-06-20 09:54:41 +0300

    create app text styles

--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -1,8 +1,27 @@
+import 'package:bookly_app/contants.dart';
 import 'package:flutter/material.dart';
 
 abstract class Styles {
-  static const titleMedium = TextStyle(
-    fontSize: 20,
+  static const textStyle18 = TextStyle(
+    fontSize: 18,
     fontWeight: FontWeight.w600,
+  );
+  static const textStyle20 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.normal,
+    fontFamily: kGtSectraFine,
+  );
+  static const textStyle14 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.normal,
+  );
+  static const textStyle16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+  );
+  static const textStyle30 = TextStyle(
+    fontSize: 30,
+    fontWeight: FontWeight.normal,
+    fontFamily: kGtSectraFine,
   );
 }
