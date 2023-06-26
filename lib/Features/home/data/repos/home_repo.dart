f0d78f0c6f3aa1c6ca4a33063b4a9f17f20412f3
Mortadha Naser <mortadha114@gmail.com>
2023-06-26 18:12:21 +0300

    create home rpoe methods and failure file

--- lib/Features/home/data/repos/home_repo.dart
+++ lib/Features/home/data/repos/home_repo.dart
@@ -1,4 +1,8 @@
+import 'package:bookly_app/Features/home/data/models/book_model/book_model.dart';
+import 'package:dartz/dartz.dart';
+import '../../../../core/errors/failures.dart';
+
 abstract class HomeRepo {
-  fetchBestSellerBooks();
-  fetchFeaturedBooks();
+ Future<Either<Failure,List<BookModel>>> fetchBestSellerBooks();
+ Future<Either<Failure,List<BookModel>>> fetchFeaturedBooks();
 }
