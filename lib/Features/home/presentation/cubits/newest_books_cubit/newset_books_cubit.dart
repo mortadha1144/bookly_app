940d9b3494c3b238960e5d9b3f0a7a76e9bf97c7
Mortadha Naser <mortadha114@gmail.com>
2023-06-26 21:59:33 +0300

    newest books cubit done

--- lib/Features/home/presentation/cubits/newest_books_cubit/newset_books_cubit.dart
+++ lib/Features/home/presentation/cubits/newest_books_cubit/newset_books_cubit.dart
@@ -1,10 +1,23 @@
-import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../data/models/book_model/book_model.dart';
+import '../../../data/repos/home_repo.dart';
 
 part 'newset_books_state.dart';
 
 class NewsetBooksCubit extends Cubit<NewsetBooksState> {
-  NewsetBooksCubit() : super(NewsetBooksInitial());
+  NewsetBooksCubit(this.homeRepo) : super(NewsetBooksInitial());
+
+  final HomeRepo homeRepo;
+
+  Future<void> fetchFeaturedBoks() async {
+    emit(NewsetBooksLoading());
+    var result = await homeRepo.fetchFeaturedBooks();
+
+    result.fold(
+      (failure) => emit(NewsetBooksFailure(failure.errMessagel)),
+      (books) => emit(NewsetBooksSuccess(books)),
+    );
+  }
 }
